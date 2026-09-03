575fc088710b80ed84862816d5f4dcfd80bbc391
SaahirT <saahirtandon@gmail.com>
2026-09-03 18:29:47 -0400

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        # gets the counts of each letter in the first word
+        counts = {}
+        for char in s:
+            if char not in counts:
+                counts[char] = 1
+            else:
+                counts[char] += 1
+
+        
+        for char in t:
+            # if a letter in the second word isn't in counts, then anagram can't be true
+            if char not in counts:
+                return False
+            # otherwise subtract an occurrence of that letter from counts
+            else:
+                counts[char] -= 1
+
+        for key in counts:
+            if counts[key] != 0:
+                return False
+
+        return True
+
+        
